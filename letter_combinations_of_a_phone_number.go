d738fc6d23dab9cb8fe789189dcf0ab3741b3fe6
GeorgiyKuzora <rafale87@gmail.com>
2024-10-24 22:05:56 +0300

    letter_combinations_of_a_phone_number.go

---
+++ letter_combinations_of_a_phone_number.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+fmt.Println(letterCombinations("hello world"))
+}
+
+func letterCombinations(digits string) []string {
+	answer := []string{digits}
+	return answer
+}
+

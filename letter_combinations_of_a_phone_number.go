d65a971d8a8d3fb5967f68d650a61e09fd59b70c
GeorgiyKuzora <rafale87@gmail.com>
2024-10-24 22:13:28 +0300

    changes

--- letter_combinations_of_a_phone_number.go
+++ letter_combinations_of_a_phone_number.go
@@ -7,6 +7,17 @@ fmt.Println(letterCombinations("hello world"))
 }
 
 func letterCombinations(digits string) []string {
+	phone_map := map[string][]string {
+		"2": []string{"a", "b", "c"},
+		"3": []string{"d", "e", "f"},
+		"4": []string{"g", "h", "i"},
+		"5": []string{"j", "k", "l"},
+		"6": []string{"m", "n", "o"},
+		"7": []string{"p", "q", "r", "s"},
+		"8": []string{"t", "u", "v"},
+		"9": []string{"w", "x", "y", "z"},
+	}
+
 	answer := []string{digits}
 	return answer
 }
